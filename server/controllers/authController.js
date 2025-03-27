858d1527f481895b4010fbd676a4fdf699a5559a
AyushMaharwade <ayushmaharwade58@gmail.com>
2025-03-27 07:46:56 +0530

    new file:   package-lock.json 	modified:   server/controllers/authController.js 	modified:   server/server.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,23 +25,33 @@ const createSendToken = (user, statusCode, res) => {
   });
 };
 
-exports.signup = catchAsync(async (req, res, next) => {
-  // Check if there are any users in the database
-  const userCount = await User.countDocuments();
+exports.signup = async (req, res) => {
+  try {
+    const userCount = await User.countDocuments();
+    const isAdmin = userCount === 0;
 
-  // Determine if the current user being added is the first user
-  const isAdmin = userCount === 0;
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      password: req.body.password,
+      isAdmin,
+    });
 
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    phoneNumber: req.body.phoneNumber,
-    password: req.body.password,
-    isAdmin,
-  });
+    createSendToken(newUser, 201, res);
+  } catch (err) {
+    console.error('Signup error:', err);
+    if (err.code === 11000) {
+      const field = Object.keys(err.keyValue)[0];
+      return res.status(400).json({
+        status: "fail",
+        message: `${field} already exists.`,
+      });
+    }
+    res.status(500).json({ status: "error", message: err.message });
+  }
+};
 
-  createSendToken(newUser, 201, res);
-});
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
